56657d02d13a579d56f4e1e5f30c00a088d44103
RESG_XX <xiao-xuan.ngew.xc@renesas.com>
2023-06-08 09:02:20 +0800

    [Update] Make portSET_INTERRUPT_MASK_FROM_ISR() a real function instead of a macro function
    Signed-off-by: RESG_XX <xiao-xuan.ngew.xc@renesas.com>

--- portable/Renesas/RX600/portmacro.h
+++ portable/Renesas/RX600/portmacro.h
@@ -125,8 +125,17 @@ extern void vTaskExitCritical( void );
 #define portEXIT_CRITICAL()		vTaskExitCritical()
 
 /* As this port allows interrupt nesting... */
-#define portSET_INTERRUPT_MASK_FROM_ISR() get_ipl(); set_ipl( ( long ) configMAX_SYSCALL_INTERRUPT_PRIORITY )
-#define portCLEAR_INTERRUPT_MASK_FROM_ISR( uxSavedInterruptStatus ) set_ipl( ( long ) uxSavedInterruptStatus )
+static int32_t set_interrupt_mask_from_isr( void );
+static int32_t set_interrupt_mask_from_isr( void )
+{
+int32_t tmp = __get_ipl();
+__set_ipl( ( long ) configMAX_SYSCALL_INTERRUPT_PRIORITY );
+return tmp;
+}
+#define portSET_INTERRUPT_MASK_FROM_ISR()
+set_interrupt_mask_from_isr()
+#define portCLEAR_INTERRUPT_MASK_FROM_ISR( uxSavedInterruptStatus )
+set_ipl( ( long ) uxSavedInterruptStatus )
 
 /*-----------------------------------------------------------*/
 
