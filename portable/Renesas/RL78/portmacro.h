8d3c890cfd6ebf5033c22a08a66caf3d499f64ab
tomohiko.watanabe.xz@bp.renesas.com <tomohiko.watanabe.xz@bp.renesas.com>
2025-10-08 02:10:54 +0000

    v11.1.0用のport/RL78を追加(対応Issue33,34,38,40)

---
+++ portable/Renesas/RL78/portmacro.h
@@ -0,0 +1,196 @@
+/*
+ * FreeRTOS Kernel V11.1.0
+ * Copyright (C) 2024 Amazon.com, Inc. or its affiliates.  All Rights Reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ * the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ * https://www.FreeRTOS.org
+ * https://github.com/FreeRTOS
+ *
+ * 1 tab == 4 spaces!
+ */
+
+#ifndef PORTMACRO_H
+#define PORTMACRO_H
+
+/*-----------------------------------------------------------
+ * Port specific definitions.
+ *
+ * The settings in this file configure FreeRTOS correctly for the
+ * given hardware and compiler.
+ *
+ * These settings should not be altered.
+ *-----------------------------------------------------------
+ */
+
+/* Type definitions - these are a bit legacy and not really used now */
+#define portCHAR                 char
+#define portFLOAT                float
+#define portDOUBLE               double
+#define portLONG                 long
+#define portSHORT                short
+#define portSTACK_TYPE           uint16_t
+#define portBASE_TYPE            short
+
+#define portSTACK_GROWTH         ( -1 )
+#define portBYTE_ALIGNMENT       2
+#define portPOINTER_SIZE_TYPE    uint16_t
+typedef portSTACK_TYPE   StackType_t;
+typedef short      		 BaseType_t;
+typedef unsigned short   UBaseType_t;
+
+#if ( configTICK_TYPE_WIDTH_IN_BITS == TICK_TYPE_WIDTH_16_BITS )
+    typedef uint16_t     TickType_t;
+    #define portMAX_DELAY    ( TickType_t ) 0xffff
+#elif ( configTICK_TYPE_WIDTH_IN_BITS == TICK_TYPE_WIDTH_32_BITS )
+    typedef uint32_t     TickType_t;
+    #define portMAX_DELAY    ( TickType_t ) 0xffffffffUL
+#elif ( configTICK_TYPE_WIDTH_IN_BITS == TICK_TYPE_WIDTH_64_BITS )
+    typedef uint64_t     TickType_t;
+    #define portMAX_DELAY    ( TickType_t ) 0xffffffffffffffffU
+#else
+    #error configTICK_TYPE_WIDTH_IN_BITS set to unsupported tick type width.
+#endif
+/*-----------------------------------------------------------*/
+
+/* Architecture specific optimisations. */
+#ifndef configUSE_PORT_OPTIMISED_TASK_SELECTION
+    #define configUSE_PORT_OPTIMISED_TASK_SELECTION    1
+#endif
+
+#if configUSE_PORT_OPTIMISED_TASK_SELECTION == 1
+
+/* Check the configuration. */
+    #if ( configMAX_PRIORITIES > 32 )
+        #error configUSE_PORT_OPTIMISED_TASK_SELECTION can only be set to 1 when configMAX_PRIORITIES is less than or equal to 32.  It is very rare that a system requires more than 10 to 15 difference priorities as tasks that share a priority will time slice.
+    #endif
+
+/* Store/clear the ready priorities in a bit map. */
+    #define portRECORD_READY_PRIORITY( uxPriority, uxReadyPriorities )    ( uxReadyPriorities ) |= ( 1UL << ( uxPriority ) )
+    #define portRESET_READY_PRIORITY( uxPriority, uxReadyPriorities )     ( uxReadyPriorities ) &= ~( 1UL << ( uxPriority ) )
+
+/*-----------------------------------------------------------*/
+
+    #define portGET_HIGHEST_PRIORITY( uxTopPriority, uxReadyPriorities ) \
+    do {                                                                 \
+        uxTopPriority = 0;                                               \
+    } while( 0 )
+
+#endif /* configUSE_PORT_OPTIMISED_TASK_SELECTION */
+
+/* Disable the interrupts */
+#define portDISABLE_INTERRUPTS()    __DI()
+
+/* Enable the interrupts */
+#define portENABLE_INTERRUPTS()     __EI()
+
+#if ( configNUMBER_OF_CORES == 1 )
+/* preserve current interrupt state and then disable interrupts */
+    #define portENTER_CRITICAL()    do {												\
+		extern volatile uint16_t usCriticalNesting;                                     \
+		                                                                                \
+		    portDISABLE_INTERRUPTS();                                                   \
+		                                                                                \
+		    /* Now interrupts are disabled ulCriticalNesting can be accessed */         \
+		    /* directly.  Increment ulCriticalNesting to keep a count of how many */    \
+		    /* times portENTER_CRITICAL() has been called. */                           \
+		    usCriticalNesting++;                                                        \
+		} while( 0 )
+
+/* restore previously preserved interrupt state */
+    #define portEXIT_CRITICAL()     do {												\
+			extern volatile uint16_t usCriticalNesting;                                 \
+			if( usCriticalNesting > portNO_CRITICAL_SECTION_NESTING )                   \
+			{                                                                           \
+				/* Decrement the nesting count as we are leaving a critical section. */ \
+				usCriticalNesting--;                                                    \
+				/* If the nesting level has reached zero then interrupts should be */   \
+				/* re-enabled. */                                                       \
+				if( usCriticalNesting == portNO_CRITICAL_SECTION_NESTING )              \
+				{                                                                       \
+					portENABLE_INTERRUPTS();                                            \
+				}                                                                       \
+			}																			\
+		} while( 0 )
+#else
+
+/* The port can maintain the critical nesting count in TCB or maintain the critical
+ * nesting count in the port. */
+    #define portCRITICAL_NESTING_IN_TCB    1
+
+/* vTaskEnterCritical and vTaskExitCritical should be used in the implementation
+ * of portENTER/EXIT_CRITICAL if the number of cores is more than 1 in the system. */
+    #define portENTER_CRITICAL             vTaskEnterCritical
+    #define portEXIT_CRITICAL              vTaskExitCritical
+
+/* vTaskEnterCriticalFromISR and vTaskExitCriticalFromISR should be used in the
+ * implementation of portENTER/EXIT_CRITICAL_FROM_ISR if the number of cores is
+ * more than 1 in the system. */
+    #define portENTER_CRITICAL_FROM_ISR    vTaskEnterCriticalFromISR
+    #define portEXIT_CRITICAL_FROM_ISR     vTaskExitCriticalFromISR
+
+#endif /* if ( configNUMBER_OF_CORES == 1 ) */
+
+/* Task utilities. */
+#define portYIELD()                                           __brk()
+#define portYIELD_FROM_ISR( xHigherPriorityTaskWoken ) if( xHigherPriorityTaskWoken ) vTaskSwitchContext()
+#define portNOP()       									  __nop()
+
+/* Task function macros as described on the FreeRTOS.org WEB site. */
+#define portTASK_FUNCTION_PROTO( vFunction, pvParameters )    void vFunction( void * pvParameters )
+#define portTASK_FUNCTION( vFunction, pvParameters )          void vFunction( void * pvParameters )
+
+#if ( configNUMBER_OF_CORES > 1 )
+    /* Return the core ID on which the code is running. */
+    #define portGET_CORE_ID()                0
+
+/* Set the interrupt mask. */
+    #define portSET_INTERRUPT_MASK()         0
+
+/* Clear the interrupt mask. */
+    #define portCLEAR_INTERRUPT_MASK( x )    ( ( void ) ( x ) )
+
+/* Request the core ID x to yield. */
+    #define portYIELD_CORE( x )              do {} while( 0 )
+
+/* Acquire the TASK lock. TASK lock is a recursive lock.
+ * It should be able to be locked by the same core multiple times. */
+    #define portGET_TASK_LOCK()              do {} while( 0 )
+
+/* Release the TASK lock. If a TASK lock is locked by the same core multiple times,
+ * it should be released as many times as it is locked. */
+    #define portRELEASE_TASK_LOCK()          do {} while( 0 )
+
+/* Acquire the ISR lock. ISR lock is a recursive lock.
+ * It should be able to be locked by the same core multiple times. */
+    #define portGET_ISR_LOCK()               do {} while( 0 )
+
+/* Release the ISR lock. If a ISR lock is locked by the same core multiple times, \
+ * it should be released as many times as it is locked. */
+    #define portRELEASE_ISR_LOCK()           do {} while( 0 )
+
+#endif /* if ( configNUMBER_OF_CORES > 1 ) */
+
+/* Critical section control macros. */
+#define portNO_CRITICAL_SECTION_NESTING        ( ( uint16_t ) 0 )
+
+/* Hardwware specifics. */
+#define portTICK_PERIOD_MS    ( ( TickType_t ) 1000 / configTICK_RATE_HZ )
+
+#endif /* PORTMACRO_H */
+
